a2af958d95ea4bd5ead2627668f913b1bad95b8e
shrihanDev <shrihankp198725@gmail.com>
2022-10-11 23:05:53 +0530

    fix: no initialization in global.d.ts

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -2,11 +2,7 @@ import { WebSocketServer } from 'ws';
 import { MiscLogger, ServerLogger, ClientLogger } from './logger';
 
 declare global {
-  var WSS: WebSocketServer | undefined = undefined;
-  var prompt: string                   = '';
-  var loggers = {
-    misc:   new MiscLogger(),
-    server: new ServerLogger(),
-    client: new ClientLogger()
-  }
+  var WSS: WebSocketServer | undefined;
+  var prompt: string;
+  var loggers: { misc: MiscLogger, server: ServerLogger, client: ClientLogger };
 }
