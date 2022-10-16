78e6aed1d47707dd5d539e50d5674ed573fc7fe3
shrihanDev <shrihankp198725@gmail.com>
2022-10-16 17:03:51 +0530

    feat: `stop` command

--- src/commands/stop.ts
+++ src/commands/stop.ts
@@ -1,3 +1,24 @@
 import Vorpal from 'vorpal';
 
-export default function (vorpal: Vorpal, _options: any): void {}
+export default function (vorpal: Vorpal): void {
+  vorpal
+    .command('stop', 'Stops the WebSocket server if its running.')
+    .alias('halt')
+    .alias('close')
+    .action(async function (this: Vorpal.CommandInstance): Promise<void> {
+      if (global.WSS === undefined) {
+        this.log(global.loggers.misc.error('The server is not running at the moment.'));
+      } else {
+        global.WSS.close((err: Error) => {
+          if (err) {
+            this.log(global.loggers.server.error(`Something wicked happened while stopping the server`));
+            this.log(global.loggers.server.trace(err));
+            return;
+          }
+          this.log(global.loggers.server.success('Successfully stopped the WebSocket server.'));
+          this.delimiter('ws-cli $');
+          global.WSS = undefined;
+        });
+      }
+    });
+}
