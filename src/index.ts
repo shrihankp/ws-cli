f07c2f0db7807aeb5681ca835362e3e7358ede10
shrihanDev <shrihankp198725@gmail.com>
2022-10-16 15:06:22 +0530

    feat: initialise global vars in app entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { textSync } from 'figlet';
+import ClientLogger from 'logger/ClientLogger';
+import MiscLogger from 'logger/MiscLogger';
+import ServerLogger from 'logger/ServerLogger';
 import { fromString as lolcat, options } from 'lolcatjs';
 import Vorpal from 'vorpal';
 import send from './commands/send';
@@ -9,6 +12,13 @@ options.animate = true;
 options.duration = 6;
 options.seed = Math.random() ** (Math.random() % Math.SQRT1_2);
 lolcat(textSync('WS-CLI', 'DOS Rebel'));
+
+global.WSS = undefined;
+global.loggers = {
+  misc: new MiscLogger(),
+  client: new ClientLogger(),
+  server: new ServerLogger()
+}
 
 const Main = new Vorpal();
 Main.use(start);
